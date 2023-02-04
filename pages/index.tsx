6e6438bd9aeb0efb32967e0021be4744177f3b61
David Xavier <davidxavier7512@gmail.com>
2023-02-04 13:12:11 +0000

    Somr

--- pages/index.tsx
+++ pages/index.tsx
@@ -164,8 +164,8 @@ export function Hero({data}: any) {
           <div className='relative w-full h-auto grid-rows-4 md:grid grid-cols-2 md:grid-rows-2 shadow-md md:-translate-y-[50%] md:-mb-[20rem] lg:-mb-[16rem]'>
             
             <div className='bg-gray-50 bg-opacity-90 px-8 py-16 flex flex-col items-center justify-center' >
-              <h2 className='text-3xl font-bold text-gray-700 uppercase mb-10 text-left'>{data[0]?.title}</h2>
-              <p className='text-gray-700 text-center font-sm'>
+              <h2 className='text-3xl font-bold text-gray-700 uppercase mb-10 text-center'>{data[0]?.title}</h2>
+              <p className='text-gray-700 text-left font-sm'>
                 {
                   parse(data[0]?.description || "")
                 }
@@ -174,15 +174,15 @@ export function Hero({data}: any) {
             </div>
 
             <div className='bg-[#f97416b9] px-8 py-16 flex flex-col items-center justify-center'>
-              <h2 className='text-3xl font-bold text-white uppercase mb-10 text-left'>{data[1]?.title}</h2>
-              <p className='text-white text-center font-sm'>
+              <h2 className='text-3xl font-bold text-white uppercase mb-10 text-center'>{data[1]?.title}</h2>
+              <p className='text-white text-left font-sm'>
                 {parse(data[1]?.description || "")}
               </p>
             </div>
 
             <div className='bg-[#45b29dd9] px-8 py-16 flex flex-col items-center justify-center'>
-              <h2 className='text-3xl font-bold text-white uppercase mb-10 text-left'>{data[2]?.title}</h2>
-              <p className='text-white text-center font-sm'>
+              <h2 className='text-3xl font-bold text-white uppercase mb-10 text-center'>{data[2]?.title}</h2>
+              <p className='text-white text-left font-sm'>
                 {parse(data[2]?.description || "")}
               </p>
             </div>
